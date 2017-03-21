667f347d8a4719734244522a39c7c00eafe11dc9
Ramesh Girigi <ramesh.kvana@gmail.com>
2017-03-21 12:05:33 +0530

    error fixed

--- app/src/test/java/com/unitjenkins/MyClassTest.java
+++ app/src/test/java/com/unitjenkins/MyClassTest.java
@@ -35,7 +35,7 @@ public class MyClassTest {
         int a = 10;
         int b = 20;
         int add = myClass.sum(a, b);
-        int result = 20;
+        int result = 30;
         assertEquals(add, result);
     }
 
